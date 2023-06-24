d40da8d7905df47292370bfd60f664de53ea8f06
m3chboy <98173890+m3chboy@users.noreply.github.com>
2023-06-24 03:50:45 +0000

    env

--- tria/__init__.py
+++ tria/__init__.py
@@ -3,6 +3,7 @@ from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import create_engine
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'jgvdfgjdf34gy43y3'
@@ -10,11 +11,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login_page"
 login_manager.login_message_category = "info"
+my_secret = os.environ['DB_Secret']
 
 Base = declarative_base()
 Session = sessionmaker()
 
-db_connection_string = "mysql+pymysql://14k0cgab5beblk2vtzi1:test@aws.connect.psdb.cloud/tria"
+db_connection_string = "mysql+pymysql://"+my_secret+"@aws.connect.psdb.cloud/tria"
 engine = create_engine(
   db_connection_string, 
   connect_args={
