39a2c30debd784b176add42b04c562afbd8c2e67
m3chboy <98173890+m3chboy@users.noreply.github.com>
2023-06-30 15:18:03 +0000

    debug of

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from tria import app
 
 #Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
